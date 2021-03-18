92fffe8f63f9d5e47f0cc7e0a7dad5b95e8b2f2e
David Alonso <david.az@hotmail.es>
2021-03-18 22:58:42 +0100

    Mission route

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <div class="card-body d-flex flex-column align-items-center py-1 mb-3">
         <h4 class="mt-0 mb-5">Welcome to Mars Rover mission control!</h4>
         <p>You can start a new mission in a randomly generated environment</p>
-        <button class="btn btn-sm btn-dark mb-5">Start a new mission</button>
+        <a class="btn btn-sm btn-dark mb-5" href="{{ route('mission') }}">Start a new mission</a>
         <p class="mb-3">Or review an already completed mission</p>
         <form method="POST">
             <div class="input-group mb-3">
---
+++ resources/views/mission.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="card-body d-flex flex-column align-items-center py-1 mb-3">
+        
+    </div>
+
+@stop
