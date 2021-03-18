f100a32a72c4e2845515430fed6b574d1362ee8e
David Alonso <david.az@hotmail.es>
2021-03-18 22:49:00 +0100

    Landing frontend & mission model

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="card-body d-flex flex-column align-items-center py-1 mb-3">
+        <h4 class="mt-0 mb-5">Welcome to Mars Rover mission control!</h4>
+        <p>You can start a new mission in a randomly generated environment</p>
+        <button class="btn btn-sm btn-dark mb-5">Start a new mission</button>
+        <p class="mb-3">Or review an already completed mission</p>
+        <form method="POST">
+            <div class="input-group mb-3">
+                <input name="id" type="text" class="form-control form-control form-control-sm" placeholder="Enter mission ID">
+                <div class="input-group-append">
+                    <button class="btn btn-dark btn-sm" type="button">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- jQuery & Bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet"> 
+
+        <style>
+            body {
+                font-family: 'Open Sans', sans-serif;
+                background: #1E1E24;
+                height: 100vh;
+            }
+            .main-title {
+                color: #11AEFA;
+            }
+        </style>
+    </head>
+    <body>
+        <main class="container d-flex h-100">
+            <div class="card mx-auto my-auto w-75">
+                <div class="card-header text-center bg-transparent border-bottom-0">
+                    <h1 class="main-title my-2">Mars Rover Mission</h1>
+                </div>
+                @yield('content')
+                <div class="card-footer text-center bg-transparent border-top-0">
+                    <span style="color: #1E1E24; font-size: 12px">Made with <span style="color: #CC1100">♥</span> by <a href="https://github.com/DvAlonso">DvAlonso</a></span>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
